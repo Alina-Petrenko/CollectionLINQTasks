b1c72d528386f176f544f601f5f445ebeae48ce3
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-11 16:13:09 +0300

    Added Third Task

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirdTask
+{
+    /// <summary>
+    /// Starts the project
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Prints the results of method call
+        /// </summary>
+        static void Main()
+        {
+            var list = new List<string>
+            {
+                "Aleksandr", "Vladyslav", "Sebastian", "Angelina","Anastasia" , "Vladimir"
+            };
+            foreach (var name in list)
+            {
+                Console.Write($"{name} ");
+            }
+            var result = GetLongest(list);
+            Console.WriteLine($"\nLongest and lexicographically first word");
+            Console.WriteLine($"{result}");
+        }
+
+        /// <summary>
+        /// Looks for the longest and lexicographically first word
+        /// </summary>
+        /// <param name="words">List of words</param>
+        /// <returns>Returns longest and lexicographically first word</returns>
+        public static string GetLongest(IEnumerable<string> words)
+        {
+                return words.Where(z => z.Length == words.Max(n => n.Length)).ToList().Min();
+             
+        }
+    }
+}
