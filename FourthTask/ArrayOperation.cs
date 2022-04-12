3ef85efead5da8056038e76a3e53abfbea679310
Alina Petrenko <alina.petrenko@globallogic.com>
2022-04-12 13:14:55 +0300

    Added FourthTask

---
+++ FourthTask/ArrayOperation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FourthTask
+{
+    /// <summary>
+    /// The class represents operations on arrays
+    /// </summary>
+    public class ArrayOperation
+    {
+        #region Public Methods
+        /// <summary>
+        /// Gets index of first non-null element in <paramref name="array"/>
+        /// </summary>
+        /// <param name="array">Array</param>
+        /// <returns>Index of first non-null element</returns>
+        public int GetIndexFirstNonNullElement (int [] array)
+        {
+            return Array
+                .IndexOf(array, (array.Where(item => item != 0)
+                .First()));
+        }
+
+        /// <summary>
+        /// Multiplies an <paramref name="array"/> by itself
+        /// </summary>
+        /// <param name="array">Array</param>
+        /// <returns>Returns result of multiply</returns>
+        public int[] MultiplyNumbers (int[] array)
+        {
+            return array.AsParallel()
+                .Select(item => item * item)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Calculates the number of matching words in a string
+        /// </summary>
+        /// <param name="array">Array</param>
+        /// <returns>Returns an array of tuples with the values word - number of repetitions</returns>
+        public (string word, int count)[] FindWordCount(string[] array)
+        {
+            return array.GroupBy(item => item, StringComparer.OrdinalIgnoreCase)
+                .Select(g => (g.Key, g.Count()))
+                .OrderBy(item => item.Key)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Connects <paramref name="firstArray"/> and <paramref name="secondArray"/> and writes values to the Dictionary, where key is a number, value is the number of repetitions of the number
+        /// </summary>
+        /// <param name="firstArray">First int array</param>
+        /// <param name="secondArray">Second int array</param>
+        /// <returns>Returns a dictionary where the key is a number from the arrays and the value is the number of repetitions of this number in these arrays</returns>
+        public Dictionary<int,int> Dictionary (int [] firstArray, int [] secondArray)
+        {
+            return firstArray.Concat(secondArray)
+                .GroupBy(item => item)
+                .OrderBy(item => item.Key)
+                .Select(group => new
+                {
+                    Key = group.Key,
+                    Value = group.Count()
+                })
+                .ToDictionary(key => key.Key, value => value.Value);
+        }
+#endregion
+    }
+}
---
+++ FourthTask/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace FourthTask
+{
+    /// <summary>
+    /// Starts the project
+    /// </summary>
+    public class Program
+    {
+        #region Public Methods
+        /// <summary>
+        /// Prints the results of method calls
+        /// </summary>
+        static void Main()
+        {
+            ArrayOperation arrayOperation = new ArrayOperation();
+            var firstIntArray = FillArrayByRandomNumbers();
+            Console.WriteLine("First Int Array");
+            IntArrayOutput(firstIntArray);
+            var secondIntArray = FillArrayByRandomNumbers();
+            Console.WriteLine("\nSecond Int Array");
+            IntArrayOutput(secondIntArray);
+
+            var multiplyElement = arrayOperation.MultiplyNumbers(firstIntArray);
+            Console.WriteLine("\nResult of Array multiplication of first array\n");
+            IntArrayOutput(multiplyElement);
+
+            var indexFirstNonNullElement = arrayOperation.GetIndexFirstNonNullElement(firstIntArray);
+            Console.WriteLine($"\nIndex of first non-null element in first Array: {indexFirstNonNullElement}");
+
+            Console.WriteLine("Dictionary");
+            var dictionary = arrayOperation.Dictionary(firstIntArray, secondIntArray);
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine($"Number: {element.Key}, Count: {element.Value} ");
+            }         
+
+            var text = "I have read your letter at e - mailfriends and would like to make friends with you! Let me introduce myself. My name is Hans. I live in Berlin. I am 20. I live with my parents. My mother is a doctor. When I feel bad, she is the first to help. I also love her apple pie, which she makes every Sunday. Cooking is her hobby. My father is a teacher His hobby is working in a little garden in front of our house. As my future profession is agronomist, I help him look after the trees and flowers. My younger brother Nick is not fond of nature. He spends ail his free time with his computer. We are both students. We study at the same university. We are all different, but when we gather together on Sundays, we can talk for hours. We discuss our family needs and plans for the future. I think we are a united and friendly family!";
+            text = text.Replace(".", String.Empty).Replace(",", String.Empty).Replace("-", String.Empty).Replace("!", String.Empty);
+            var stringArray = text.Split(" ");
+
+            Console.WriteLine("\nText");
+            var wordCount = arrayOperation.FindWordCount(stringArray);
+            Console.WriteLine(text);
+            Console.WriteLine("\nWord Count:");
+            foreach (var element in wordCount)
+            {
+                Console.WriteLine($"Word: {element.word}, Count: {element.count} ");
+            }
+
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Fills array by random numbers
+        /// </summary>
+        /// <returns>Returns array with random numbers</returns>
+        private static int[] FillArrayByRandomNumbers ()
+        {
+            Random random = new Random();
+            var array = new int[random.Next(1, 1001)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 101);
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Writes <paramref name="array"/> to the console
+        /// </summary>
+        /// <param name="array">Array</param>
+        private static void IntArrayOutput (int [] array)
+        {
+            foreach (var element in array)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+        #endregion
+    }
+}
